ea286a00d45f85602cc58acc11bd1344dd0161e8
Vitali Carbivnicii <realcnbs@gmail.com>
2022-02-25 18:16:35 -0500

    Don't poll notifications on document:visibilitychange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { NotificationsService } from '@picthor/notifications/notifications.service';
-import { Subscription, timer } from 'rxjs';
-import { mergeAll, mergeMap, tap } from 'rxjs/operators';
+import { fromEvent, interval, NEVER, Subscription } from 'rxjs';
+import { distinctUntilChanged, map, mergeAll, mergeMap, shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,27 +11,40 @@ import { mergeAll, mergeMap, tap } from 'rxjs/operators';
 export class AppComponent implements OnDestroy {
   title = 'picthor-io';
 
-  notificationsObservable = timer(0, 5000).pipe(
-    mergeMap(() => this.notifications.getAll()),
-    mergeAll(),
-    tap((notification) => {
-      switch (notification.type) {
-        case 'INFO':
-          this.toastr.info(notification.message, notification.title);
-          break;
-        case 'SUCCESS':
-          this.toastr.success(notification.message, notification.title);
-          break;
-        case 'ERROR':
-          this.toastr.error(notification.message, notification.title);
-          break;
-      }
-    })
+  private visibilityChange$ = fromEvent(document, 'visibilitychange').pipe(
+    map((x) => document.visibilityState),
+    startWith('visible'),
+    shareReplay(1)
   );
+
+  private isVisible$ = this.visibilityChange$.pipe(
+    map((x) => x === 'visible'),
+    distinctUntilChanged()
+  );
+
   notifications$?: Subscription;
 
   constructor(private toastr: ToastrService, private notifications: NotificationsService) {
-    this.notifications$ = this.notificationsObservable.subscribe();
+    this.notifications$ = this.isVisible$
+      .pipe(
+        switchMap((visible) => (visible ? interval(2000) : NEVER)),
+        mergeMap(() => this.notifications.getAll()),
+        mergeAll(),
+        tap((notification) => {
+          switch (notification.type) {
+            case 'INFO':
+              this.toastr.info(notification.message, notification.title);
+              break;
+            case 'SUCCESS':
+              this.toastr.success(notification.message, notification.title);
+              break;
+            case 'ERROR':
+              this.toastr.error(notification.message, notification.title);
+              break;
+          }
+        })
+      )
+      .subscribe();
   }
 
   ngOnDestroy(): void {
