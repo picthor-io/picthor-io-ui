20e168a19722f733bfaf50519497987fa0915ec6
Vitali Carbivnicii <realcnbs@gmail.com>
2022-09-27 22:17:52 -0400

    Minor sync job status improvements

--- src/app/roots/root-card.component.ts
+++ src/app/roots/root-card.component.ts
@@ -6,6 +6,7 @@ import { JobCounter } from '@picthor/batch-job/job-counter';
 import { RxStompService } from '@picthor/rx-stomp.service';
 import { filter, map, tap } from 'rxjs/operators';
 import { Message } from '@stomp/stompjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root-card',
@@ -16,21 +17,27 @@ export class RootCardComponent implements OnDestroy {
   directory!: Directory;
 
   syncJobs: BatchJob[] = [];
+  private jobCounterUpdate$: Subscription;
+  private jobAdd$: Subscription;
+  private jobRemove$: Subscription;
 
   constructor(protected directoriesService: DirectoriesService, private rxStompService: RxStompService) {
-    this.rxStompService.watch('/topic/jobs/add').pipe(
+
+    let directoryUpdateTimer: any = null;
+
+    this.jobAdd$ = this.rxStompService.watch('/topic/jobs/add').pipe(
       map<Message, BatchJob>(message => Object.assign(new BatchJob(), JSON.parse(message.body))),
       filter(job => job.rootDirectoryId === this.directory.id),
       tap(job => this.syncJobs.push(job))
     ).subscribe();
 
-    this.rxStompService.watch('/topic/jobs/remove').pipe(
+    this.jobRemove$ = this.rxStompService.watch('/topic/jobs/remove').pipe(
       map<Message, BatchJob>(message => Object.assign(new BatchJob(), JSON.parse(message.body))),
       filter(job => job.rootDirectoryId === this.directory.id),
       tap(job => this.syncJobs = this.syncJobs.filter(sj => sj.id !== job.id))
     ).subscribe();
 
-    this.rxStompService.watch('/topic/jobs/counter-update').pipe(
+    this.jobCounterUpdate$ = this.rxStompService.watch('/topic/jobs/counter-update').pipe(
       map<Message, JobCounter>(message => Object.assign(new JobCounter(), JSON.parse(message.body))),
       filter(counter => counter.rootDirectoryId === this.directory.id),
       tap(counter => {
@@ -39,13 +46,22 @@ export class RootCardComponent implements OnDestroy {
             this.syncJobs[index].counter = counter;
           }
         });
+        if (this.directory.stats.sizeBytes === 0 && directoryUpdateTimer == null) {
+          directoryUpdateTimer = setTimeout(() => {
+            this.directoriesService.getById(this.directory.id).subscribe(d => {
+              this.directory = d
+              directoryUpdateTimer = null;
+            });
+          }, 5000);
+        }
       })
     ).subscribe();
   }
 
   ngOnDestroy(): void {
-    // this.jobRemoved$?.unsubscribe();
-    // this.jobAdded$?.unsubscribe();
+    this.jobAdd$.unsubscribe();
+    this.jobRemove$.unsubscribe();
+    this.jobCounterUpdate$.unsubscribe();
   }
 
   syncDirectory(directory: Directory) {
